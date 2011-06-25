1c598548a1b2764192af26acc67df98732516b70
Sakari Jokinen <sakariij@gmail.com>
2011-06-25 18:08:04 +0300

    refactored shooting to 'trooper'

--- src/trooper.js
+++ src/trooper.js
@@ -40,9 +40,6 @@ Crafty.c('TrooperControl', {
 	     }
 	     , TrooperControl : function() {
 		 var current_direction_rad = 0;
-		 var shoot_delay_ms = 500;
-		 var last_shot_ms = 0;
-		 
 		 return this
 		     .multiway(1, { UP_ARROW : -90, DOWN_ARROW : 90, RIGHT_ARROW : 0, LEFT_ARROW : 180} )
 		     .bind("NewDirection", function(movement) {
@@ -54,18 +51,7 @@ Crafty.c('TrooperControl', {
 			       if (e.key != Crafty.keys.A) {
 				   return;
 			       }
-			       var current_time = new Date().getTime();
-			       var deviation_rad = 0.2;
-
-			       if (last_shot_ms + shoot_delay_ms < current_time) {
-				   last_shot_ms = current_time;
-			     	   Crafty.e('2D, DOM, bullet, Bullet')
-				       .attr({ x: this.x + 3 , y: this.y + 5, z: 3 })
-				       .Bullet(Trig.fuzzy_angle(current_direction_rad, deviation_rad)
-					       , 5
-					       , 30
-					       , this);
-			       }
+			       this.trigger('trooper.shoot', current_direction_rad);
 			   });
 	     }
 });
@@ -80,7 +66,7 @@ Crafty.c('ai_trooper', {
 	     init : function() {
 		 this.requires("SpriteAnimation, target");
 	     }
-	     ,ai_trooper : function() {
+	     , ai_trooper : function() {
 		 return this;
 	     }
 	     , shot : function() {
@@ -94,8 +80,24 @@ Crafty.c('trooper', {
 		 this.requires("SpriteAnimation, target, TrooperControl");
 	     },
 	     trooper :  function() {
+		 var last_shot_ms = 0;
+		 var shoot_delay_ms = 300;
+		 var deviation_rad = 0.2;
+
 		 return this.animate("walk", 0, 0, 3)
 		     .TrooperControl()
+		     .bind('trooper.shoot', function(direction) {
+			       var current_time = new Date().getTime();
+			       if (last_shot_ms + shoot_delay_ms < current_time) {
+				   last_shot_ms = current_time;
+			     	   Crafty.e('2D, DOM, bullet, Bullet')
+				       .attr({ x: this.x + 3 , y: this.y + 5, z: 3 })
+				       .Bullet(Trig.fuzzy_angle(direction, deviation_rad)
+					       , 5
+					       , 30
+					       , this);
+			       }
+  			   })
 		     .bind("NewDirection", function(direction) {
 			       if (!direction.x && !direction.y)
 				   this.stop();
