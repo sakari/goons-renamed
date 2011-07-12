0cb98bdf4511d64d0eb0eff8ca0a403ca1d6bf2f
Sakari Jokinen <sakariij@gmail.com>
2011-07-12 21:55:50 +0300

    refactored world generation
    separate file in preparation for hacking on it

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -1,26 +1,7 @@
 Crafty.scene("main", function () {
-		 function generateWorld() {
-		     Crafty.sprite(16, "sprites/ground.png", {
-				       ground0 : [0, 0],
-				       ground1 : [1, 0],
-				       ground2 : [2, 0],
-				       ground3 : [3, 0],
-				       ground4 : [4, 0],
-				       ground5 : [5, 0],
-				       ground6 : [6, 0],
-				       ground7 : [7, 0],
-				       ground8 : [8, 0],
-				       ground9 : [9, 0]
-				   });
-		     for (var i = 0; i < 25; i++) {
-			 for (var j = 0; j < 21; j++) {
-			     var groundType = Crafty.randRange(0, 9);
-			     Crafty.e("2D, DOM, ground" + groundType)
-				 .attr({ x: i * 16, y: j * 16, z:1 });
-			 }
-		     }
-		 }
+		 //##src/world.js
 		 generateWorld();
+
 		 //##src/trooper.js
 		 var player = Crafty.e("2D, DOM, blue_trooper, player_trooper")
 		     .attr({ x: 100, y: 100, z:2 })
---
+++ src/world.js
@@ -0,0 +1,21 @@
+function generateWorld() {
+    Crafty.sprite(16, "sprites/ground.png", {
+		      ground0 : [0, 0],
+		      ground1 : [1, 0],
+		      ground2 : [2, 0],
+		      ground3 : [3, 0],
+		      ground4 : [4, 0],
+		      ground5 : [5, 0],
+		      ground6 : [6, 0],
+		      ground7 : [7, 0],
+		      ground8 : [8, 0],
+		      ground9 : [9, 0]
+		  });
+    for (var i = 0; i < 25; i++) {
+	for (var j = 0; j < 21; j++) {
+	    var groundType = Crafty.randRange(0, 9);
+	    Crafty.e("2D, DOM, ground" + groundType)
+		.attr({ x: i * 16, y: j * 16, z:1 });
+	}
+    }
+}
